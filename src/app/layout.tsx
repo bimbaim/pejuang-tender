02282b2df396bb5a48c8c0ba253f949647b2efad
bimbaim <ibrahimquraisy@gmail.com>
2025-09-23 09:35:50 +0700

    refactor: Add GA4 dan GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+// app/layout.tsx
+
 import type { Metadata } from 'next';
 import { Saira } from 'next/font/google';
 import { Quicksand } from 'next/font/google';
-import Script from 'next/script'; // Import the Script component
 import './globals.css';
+
+// Import komponen GTM
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const saira = Saira({ subsets: ['latin'] });
 const quicksand = Quicksand({ subsets: ['latin'] });
@@ -11,7 +15,7 @@ export const metadata: Metadata = {
   title: 'Pejuang Tender',
   description: 'Sistem notifikasi tender otomatis',
   icons: {
-    icon: '/logo-footer.png', // Favicon
+    icon: '/logo-footer.png',
   },
 };
 
@@ -22,23 +26,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-CRE8KHKB2J"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-CRE8KHKB2J');
-          `}
-        </Script>
-      </head>
       <body className={`${saira.className} ${quicksand.className}`}>
         {children}
       </body>
+      
+      {/* Tambahkan Google Tag Manager di sini */}
+      <GoogleTagManager gtmId="GTM-5VLSLXN3" />
     </html>
   );
 }
