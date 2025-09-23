de8fc7062379d4b72f7ef5410cd1dd74d7505bd9
bimbaim <ibrahimquraisy@gmail.com>
2025-09-23 09:25:05 +0700

    Add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next';
 import { Saira } from 'next/font/google';
 import { Quicksand } from 'next/font/google';
+import Script from 'next/script'; // Import the Script component
 import './globals.css';
-
-
 
 const saira = Saira({ subsets: ['latin'] });
 const quicksand = Quicksand({ subsets: ['latin'] });
@@ -23,6 +22,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-CRE8KHKB2J"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CRE8KHKB2J');
+          `}
+        </Script>
+      </head>
       <body className={`${saira.className} ${quicksand.className}`}>
         {children}
       </body>
