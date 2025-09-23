71c03a5b19860f2337254f39c415b628c16f1c63
bimbaim <ibrahimquraisy@gmail.com>
2025-09-23 11:22:46 +0700

    Update global datalayaer

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -2,8 +2,7 @@
 export {};
 
 declare global {
-  // Antarmuka untuk item e-commerce, semua properti bersifat opsional
-  // untuk mengakomodasi berbagai event (misal: view_item_list vs. add_to_cart)
+  // Antarmuka untuk item e-commerce
   interface DataLayerItem {
     item_id?: string;
     item_name?: string;
@@ -13,25 +12,30 @@ declare global {
     item_list_name?: string;
   }
 
-  // Antarmuka untuk objek e-commerce, digunakan oleh event seperti
+  // Antarmuka untuk objek e-commerce yang digunakan oleh
   // 'view_cart', 'add_to_cart', 'begin_checkout', dan 'purchase'
   interface StandardEcommerceData {
     currency: string;
     value: number;
     items: DataLayerItem[];
+    // Menambahkan properti spesifik untuk event purchase
+    transaction_id?: string;
+    affiliation?: string;
+    tax?: number;
+    shipping?: number;
   }
 
-  // Antarmuka untuk event 'view_item_list', yang memiliki struktur berbeda
+  // Antarmuka untuk event 'view_item_list'
   interface ViewItemListEcommerceData {
     items: DataLayerItem[];
     item_list_id: string;
     item_list_name: string;
   }
 
-  // Union type yang menggabungkan semua struktur event yang mungkin
+  // Union type yang menggabungkan semua struktur event
   type DataLayerEvent =
     | { event: "view_item_list"; ecommerce: ViewItemListEcommerceData }
-    | { event: "add_to_cart" | "begin_checkout" | "view_cart"| "purchase"; ecommerce: StandardEcommerceData };
+    | { event: "add_to_cart" | "begin_checkout" | "view_cart" | "purchase"; ecommerce: StandardEcommerceData };
 
   // Mendeklarasikan window.dataLayer sebagai array dari union type yang baru
   interface Window {
