fecc6be14d473becc59ca2b573671c98c58c0554
bimbaim <ibrahimquraisy@gmail.com>
2025-09-24 09:50:15 +0700

    Update supabaseKey to use Anon in Email webhook

--- src/app/api/send-daily-paid-email/route.ts
+++ src/app/api/send-daily-paid-email/route.ts
@@ -4,7 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // --- Inisialisasi Klien Supabase ---
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+// const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: false,
